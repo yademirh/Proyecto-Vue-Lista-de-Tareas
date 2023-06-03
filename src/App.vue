2161829561b4176fff5d73921b1e0ae417a66a0a
Yademir Hernandez <yhernandezs@ufpso.edu.co>
2023-06-03 12:27:18 -0500

    Lo que se hizo en clase

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+  import TaskComponent from '@/components/TaskComponent.vue';
+  import { ref } from 'vue';
+
+  let newTask;
+
+  const tasksList = ref([
+    'Levantarse',
+    'Cruzar el semáforo sin mirar',
+    'Salvese quien pueda'
+  ])
+
+  const deleteTaskarray = (index) => {
+    // console.log('Kill ', index)
+    console.log('Kill ', tasksList)
+    tasksList.value.splice(index, 1)
+  }
+
+  const addTask = () => {
+    tasksList.value.unshift(newTask);  //o tambien .push
+    newTask = undefined   
+  }
+</script>
+
+<template>
+
+  <div class="font-medium border-2 border-[#ededed] shadow-lg mt-[250px] mx-auto w-[450px] px-8 py-5 bg-gray-50 space-y-2 overflow-hidden">
+    <h1 class="text-2xl font-bold text-center">Lista de Tareas</h1>
+    <div class="rounded-md overflow-hidden h-9  mx-auto flex border-2      border-black">
+      <input v-model="newTask" class="w-4/5 outline-none px-2 py-1" type="text" placeholder="Actividad...">
+      <span @click="addTask" class="hover:cursor-pointer bg-blue-500 hover:bg-blue-600 duration-100 border-l-2 border-black w-1/5 flex justify-center items-center text-xl " >
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></span>
+    </div>
+
+    <TaskComponent v-for="(task,index) in tasksList" :key="index" :nombre="task" :numero="index" @borrarTask="deleteTaskarray" />
+
+  </div>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
+ 
+
+ 
---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+const props = defineProps({
+    nombre: {
+        type: String,
+        default: 'No tengo nombre'
+    },
+    numero: {
+        type: Number
+    }
+})
+
+const emit = defineEmits([
+    'borrarTask'
+])
+
+const deleteTask = (numero) => {
+    console.log('Borrrar ',numero);
+    emit('borrarTask', numero);
+}
+
+</script>
+
+<template>
+    <div class="flex justify-between mx-auto h-9 px-4 py-2 bg-gray-200 rounded items-center">
+       
+        {{ props.nombre }}
+
+        <span class="hover:cursor-pointer h-full flex items-center" @click="deleteTask(props.numero)">
+            <svg class="text-red-600 hover:text-red-800 duration-100 fill-current h-full" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm79 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>
+        </span>
+    </div>
+    
+</template>
